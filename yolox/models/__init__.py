e2bc65b816321fb163daf7b592311b35b32182f6
zhanglibin.buaa <zhanglibin.buaa@bytedance.com>
2022-04-21 13:09:23 +0800

    add alpha_iouLoss

--- yolox/models/__init__.py
+++ yolox/models/__init__.py
@@ -3,7 +3,7 @@
 # Copyright (c) 2014-2021 Megvii Inc. All rights reserved.
 
 from .darknet import CSPDarknet, Darknet
-from .losses import IOUloss
+from .losses import IOUloss, alpha_IOUloss
 from .yolo_fpn import YOLOFPN
 from .yolo_head import YOLOXHead
 from .yolo_pafpn import YOLOPAFPN
